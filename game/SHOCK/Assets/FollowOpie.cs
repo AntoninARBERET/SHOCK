e52e38e2c0f6615947eeace8899b045f0b89eaa3
Asma <asmabrazi27@gmail.com>
2019-12-20 12:08:30 +0100

    cat OK

---
+++ game/SHOCK/Assets/FollowOpie.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowOpie : MonoBehaviour
+{
+  public GameObject Player;
+  public float movementSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+{
+    transform.LookAt(Player.transform);
+    transform.position += transform.forward * movementSpeed * Time.deltaTime;
+}
+}
