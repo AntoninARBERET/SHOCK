c8e6244ff027d4877170d25fecff1673245bd61b
Asma <asmabrazi27@gmail.com>
2019-12-25 09:58:12 +0100

    open door anim OK

--- game/SHOCK/Assets/Open.cs
+++ game/SHOCK/Assets/Open.cs
@@ -3,35 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Open : MonoBehaviour
-{	public Transform player;
-	public bool openDoor=false;
-    // Start is called before the first frame update
-    void Start()
-    {
+{	
+    Animator anim;
+
+    // Use this for initialization
+    void Start () {
+
+        anim = GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        open();
+    void Update () {
+        
     }
-    public void open(){
-    	if( openDoor==false){
-        	getClosestPlayer(player);
-        	openDoor=true;
-        }
-    }
-    public void getClosestPlayer (Transform player){
-        Vector3 currentPosition = transform.position;
-        Vector3 directionToTarget = player.position - currentPosition;
-            float dSqrToTarget = directionToTarget.sqrMagnitude;
-            if(dSqrToTarget < 3)
-            {if (Input.GetMouseButtonDown(0)){
-                GetComponent<Animator>().enabled = true;
-    			//Debug.Log("jr");
-            }
-        }
 
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("OpenDoor");
     }
+
+    void OnTriggerExit(Collider other)
+    {
+        anim.enabled = true;
+    }
+
+    void pauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+
+
 }
