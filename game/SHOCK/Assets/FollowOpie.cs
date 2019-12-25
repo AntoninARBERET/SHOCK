d5eb7a2471070f9bb925cee3f810ac036f7f1022
Asma <asmabrazi27@gmail.com>
2019-12-25 16:46:49 +0100

    cat move to ok :D !!

--- game/SHOCK/Assets/FollowOpie.cs
+++ game/SHOCK/Assets/FollowOpie.cs
@@ -5,31 +5,80 @@ using UnityEngine;
 public class FollowOpie : MonoBehaviour
 {
   public GameObject player;
+  public Transform p1;
+  public Transform p2;
   float target_dist;
   public float allowed_dist=0.75f;
   float speed;
   RaycastHit shot;
-
+  private bool calibration=true;
+  private bool gotoP1=true;
+  private bool gotoP2=false;
+  private bool waiting=false;
+  private Vector3 targetPosition1,targetPosition2;
     // Start is called before the first frame update
     void Start()
     {
-
+       targetPosition1=new Vector3(4.80f,transform.position.y, 17.0f);
+       targetPosition2=new Vector3(3.0f,transform.position.y, 17.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-      transform.LookAt(player.transform);
-      if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out shot)){
-        target_dist=shot.distance;
-        if(target_dist>=allowed_dist){
-          speed=0.1f;
-          transform.position =Vector3.MoveTowards(transform.position,player.transform.position,speed);
-        }
-        
-        else{
-          speed=0.0f;
+      if(!calibration){
+        transform.LookAt(player.transform);
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out shot)){
+          target_dist=shot.distance;
+          if(target_dist>=allowed_dist){
+            speed=0.1f;
+            transform.position =Vector3.MoveTowards(transform.position,player.transform.position,speed);
+          }
+
+          else{
+            speed=0.0f;
+          }
         }
       }
+      else{
+        if(gotoP1){
+          inFrontOfHouse();
+          if(transform.position.Equals(targetPosition1)){
+            gotoP1=false;
+            waiting=true;
+          }
+        }
+        if(waiting){
+          StartCoroutine(waitForAWhile());
+        }else{
+          if(gotoP2){
+            EnterHouse();
+            if(transform.position.Equals(targetPosition2)){
+              gotoP2=false;
+            }
+          }
+        }
+
+      }
+  }
+
+  private void inFrontOfHouse(){
+    transform.LookAt(p1);
+     GetComponent<Animator>().Play("Walk");
+     transform.position = Vector3.MoveTowards(transform.position, targetPosition1, 0.1f);
+     GetComponent<Animator>().Play("Idle");
+
+  }
+  IEnumerator waitForAWhile(){
+        yield return new WaitForSeconds(3f);
+        waiting=false;
+        gotoP2=true;
+    }
+    public void EnterHouse(){
+          transform.LookAt(p2);
+          GetComponent<Animator>().Play("Walk");
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition2, 0.1f);
+           GetComponent<Animator>().Play("Idle");
+
   }
 }
