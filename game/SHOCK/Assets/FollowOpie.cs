b06291d68a1097176541099f64b9bb5ef6780b88
Asma <asmabrazi27@gmail.com>
2019-12-25 09:05:31 +0100

    follow ajusted

--- game/SHOCK/Assets/FollowOpie.cs
+++ game/SHOCK/Assets/FollowOpie.cs
@@ -6,7 +6,7 @@ public class FollowOpie : MonoBehaviour
 {
   public GameObject player;
   float target_dist;
-  float allowed_dist=1f;
+  public float allowed_dist=0.75f;
   float speed;
   RaycastHit shot;
 
@@ -17,14 +17,17 @@ public class FollowOpie : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
       transform.LookAt(player.transform);
       if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out shot)){
         target_dist=shot.distance;
         if(target_dist>=allowed_dist){
-          speed=0.25f;
+          speed=0.1f;
           transform.position =Vector3.MoveTowards(transform.position,player.transform.position,speed);
+        }
+        else{
+          speed=0.0f;
         }
       }
   }
